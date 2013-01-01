4d6d7d827023ab12bfc3384fa1e54a2c7b352cdf
irof <irof@hogedriven.net>
2013-01-01 23:40:24 +0900

    投入金額取得、いきなりGREENだけど良しとする

---
+++ src/main/java/tddbc/VendingMachine.java
@@ -0,0 +1,9 @@
+package tddbc;
+
+public class VendingMachine {
+    private int creditAmount;
+
+    public int getCreditAmount() {
+        return creditAmount;
+    }
+}
--- src/test/java/tddbc/VendingMachineTest.java
+++ src/test/java/tddbc/VendingMachineTest.java
@@ -1,4 +1,16 @@
 package tddbc;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class VendingMachineTest {
+
+    @Test
+    public void 投入前に投入金額を取得したら0円() {
+        VendingMachine sut = new VendingMachine();
+        int actual = sut.getCreditAmount();
+        assertThat(actual, is(0));
+    }
 }
