d780a8ebd419b65e0520f1a69f9afbc54a624bcf
irof <irof@hogedriven.net>
2013-01-01 23:51:11 +0900

    テストコードのリファクタリング

--- src/test/java/tddbc/VendingMachineTest.java
+++ src/test/java/tddbc/VendingMachineTest.java
@@ -1,5 +1,6 @@
 package tddbc;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -7,18 +8,21 @@ import static org.junit.Assert.assertThat;
 
 public class VendingMachineTest {
 
+    private VendingMachine sut;
+
+    @Before
+    public void setUp() {
+        sut = new VendingMachine();
+    }
+
     @Test
     public void 投入前に投入金額を取得したら0円() {
-        VendingMachine sut = new VendingMachine();
-        int actual = sut.getCreditAmount();
-        assertThat(actual, is(0));
+        assertThat(sut.getCreditAmount(), is(0));
     }
 
     @Test
     public void 投入後は投入金額が取得できる() {
-        VendingMachine sut = new VendingMachine();
         sut.insert(100);
-        int actual = sut.getCreditAmount();
-        assertThat(actual, is(100));
+        assertThat(sut.getCreditAmount(), is(100));
     }
 }
