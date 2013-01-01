02d46be8aaf181b2f2abf478f688f9d76dcce557
irof <irof@hogedriven.net>
2013-01-01 23:58:18 +0900

    テストクラスをEnclosedにする

--- src/test/java/tddbc/VendingMachineTest.java
+++ src/test/java/tddbc/VendingMachineTest.java
@@ -2,40 +2,45 @@ package tddbc;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(Enclosed.class)
 public class VendingMachineTest {
 
-    private VendingMachine sut;
+    public static class SimpleOperation {
+        private VendingMachine sut;
 
-    @Before
-    public void setUp() {
-        sut = new VendingMachine();
-    }
+        @Before
+        public void setUp() {
+            sut = new VendingMachine();
+        }
 
-    @Test
-    public void 投入前に投入金額を取得したら0円() {
-        assertThat(sut.getCreditAmount(), is(0));
-    }
+        @Test
+        public void 投入前に投入金額を取得したら0円() {
+            assertThat(sut.getCreditAmount(), is(0));
+        }
 
-    @Test
-    public void 投入後は投入金額が取得できる() {
-        sut.insert(100);
-        assertThat(sut.getCreditAmount(), is(100));
-    }
+        @Test
+        public void 投入後は投入金額が取得できる() {
+            sut.insert(100);
+            assertThat(sut.getCreditAmount(), is(100));
+        }
 
-    @Test
-    public void 複数回投入して投入金額を取得() {
-        sut.insert(100);
-        sut.insert(100);
-        assertThat(sut.getCreditAmount(), is(200));
-    }
+        @Test
+        public void 複数回投入して投入金額を取得() {
+            sut.insert(100);
+            sut.insert(100);
+            assertThat(sut.getCreditAmount(), is(200));
+        }
 
-    @Test
-    public void 無効なお金は受け付けない() {
-        sut.insert(5);
-        assertThat(sut.getCreditAmount(), is(0));
+        @Test
+        public void 無効なお金は受け付けない() {
+            sut.insert(5);
+            assertThat(sut.getCreditAmount(), is(0));
+        }
     }
 }
